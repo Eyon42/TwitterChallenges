49c2707f0a0b7c13e000603604700eb9ca736a52
Francesco Gentile <francescogentyle@gmail.com>
2023-02-09 19:38:49 -0300

    Mate re-branding

--- day3-mate-tracker/src/App.jsx
+++ day3-mate-tracker/src/App.jsx
@@ -12,8 +12,21 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="bg-[url('/mate-background.avif')] bg-cover bg-fixed flex flex-col align-center items-center gap-8 py-8 px-4 w-full m-0 h-full min-h-screen">
-        <MateApp />
+      <div className="bg-dark pt-8 px-4 w-full m-0 h-full min-h-screen text-white">
+        <main className="min-h-screen">
+          <MateApp />
+        </main>
+        <div className="flex w-full items-center justify-center mt-8 pb-2">
+          <p className="text-gray-500">
+            Hecho por{" "}
+            <a
+              href="https://bestem.dev"
+              className="underline hover:text-accent transition-colors"
+            >
+              Bestem.dev
+            </a>
+          </p>
+        </div>
       </div>
     </QueryClientProvider>
   );
@@ -63,8 +76,8 @@ const MateApp = () => {
 
   return (
     <>
-      <div className="flex gap-8 items-center justify-center flex-col align-middle">
-        <img src="/mate.avif" alt="Mate" className="h-64 rounded-full w-64" />
+      <div className="flex gap-12 items-center justify-center flex-col align-middle">
+        <img src="/mate.svg" alt="Mate" className="h-64 rounded-full w-64" />
         <CurrentMateHolder holderAddress={tokenDataQuery.data?.holder} />
         <HistoricBoard holderHistory={tokenDataQuery.data?.history} />
       </div>
@@ -79,29 +92,60 @@ const CurrentMateHolder = (props) => {
   };
 
   return (
-    <div className="flex flex-col items-center text-gray-900 bg-gray-200/90 rounded-3xl p-5 gap-4">
-      <h1 className="text-4xl font-bold text-center">
+    <>
+      <h1 className="text-2xl text-center font-gradient accent-gradient uppercase">
         ¿Quién tiene el mate ahora?
       </h1>
-      <MateHolder holder={holder} />
-    </div>
+      <div className="flex flex-col gap-3">
+        <div className="px-1 w-full">
+          <div className="flex flex-row items-center text-gray-900rounded-3xl px-8 gap-6 bg-dark rounded-3xl">
+            <p className="w-24 xs:w-32 sm:w-96 text-center">Address:</p>
+            <div></div>
+            <img
+              src="/welook.png"
+              alt="WeLook"
+              className="ml-6 xs:ml-8 h-6 w-auto"
+            />
+          </div>
+        </div>
+        <div className=" bg-gradient-to-r from-secondary to-accent rounded-3xl p-0.5">
+          <div className="flex flex-col items-center text-gray-900rounded-3xl py-4 px-8 gap-4 bg-dark rounded-3xl">
+            <MateHolder holder={holder} />
+          </div>
+        </div>
+      </div>
+    </>
   );
+};
+
+// utility to shorten an address
+const shortenAddress = (address) => {
+  return address.slice(0, 6) + "..." + address.slice(-4);
 };
 
 const MateHolder = ({ holder }) => {
   return (
-    <div className="flex flex-col items-center gap-2">
+    <div className="flex flex-row items-center gap-8">
       {holder.ens && (
         <h1 className="text-4xl font-bold text-center">{holder.ens}</h1>
       )}
-      <p className="text-center">
+      <p className="text-center w-24 xs:w-32 sm:w-96">
         <a
           href={"https://polygonscan.com/address/" + holder.address}
-          className="hover:text-blue-400 hover:underline"
+          className="hover:text-accent text-gray-400 underline transition-colors"
         >
-          {holder.address}
+          <span className="block sm:hidden">
+            {shortenAddress(holder.address)}
+          </span>
+          <span className="sm:block hidden">{holder.address}</span>
         </a>
       </p>
+      <div className="border-r border h-8 border-gray-600/80"></div>
+      <a href={"https://welook.io/" + holder.address}>
+        <button className="border border-1 rounded-full py-2 sm:w-[120px] px-4 xs:px-6 border-primary hover:bg-primary hover:font-bold transition-colors">
+          Ver Perfil
+        </button>
+      </a>
     </div>
   );
 };
@@ -113,12 +157,31 @@ const HistoricBoard = (props) => {
   }));
   console.log(holders);
   return (
-    <div className="w-full flex flex-col items-center text-gray-900 bg-gray-200/90 rounded-3xl p-5 gap-4">
-      <h1 className="text-4xl font-bold text-center">¿Quién ya tomó mate?</h1>
-      {holders?.map((holder) => (
-        <MateHolder holder={holder} />
-      ))}
-    </div>
+    <>
+      <h1 className="text-2xl text-center font-gradient accent-gradient uppercase">
+        ¿Quién ya tomó mate?
+      </h1>
+      <div className="flex flex-col gap-3">
+        <div className="px-1 w-full">
+          <div className="flex flex-row items-center text-gray-900rounded-3xl px-8 gap-6 bg-dark rounded-3xl">
+            <p className="w-24 xs:w-32 sm:w-96 text-center">Address:</p>
+            <div></div>
+            <img
+              src="/welook.png"
+              alt="WeLook"
+              className="ml-6 xs:ml-8 h-6 w-auto"
+            />
+          </div>
+        </div>
+        <div className=" bg-gradient-to-r from-secondary to-accent rounded-3xl p-0.5">
+          <div className="flex flex-col items-center text-gray-900rounded-3xl py-6 px-8 gap-6 bg-dark rounded-3xl">
+            {holders?.map((holder) => (
+              <MateHolder holder={holder} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- day3-mate-tracker/tailwind.config.cjs
+++ day3-mate-tracker/tailwind.config.cjs
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "412px",
+      sm: "670px",
+    },
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#DA5598",
+        secondary: "#5D0EC1",
+        accent: "#52FCA9",
+        dark: "#111729",
+      },
+    },
   },
   plugins: [],
-}
+};
